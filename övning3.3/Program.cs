ea83539dab802093b002d8b524e83dfff2bb4c4b
emsj06001 <emsj06001@E-PW024NWP>
2023-10-04 10:58:49 +0200

    Add project files.

---
+++ övning3.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace uppgift_3_3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I hur många timmar vill du hyra bil?...");
+            string h = Console.ReadLine();
+            int timmar = int.Parse(h);
+
+            int sum = timmar * 80;
+            
+            if (sum > 950)
+            {
+                Console.WriteLine("Det kostar 950 kr");
+            }
+            else
+            {
+                Console.WriteLine("Det kostar "+ sum +" kr");
+            }
+            Console.ReadKey();
+        }
+    }
+}
